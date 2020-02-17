f479d6afdc233b48a6ca57b7945a4b87ac953ff4
Alyssa Bryant <alyssa.g.bryant@gmail.com>
2020-02-16 18:36:36 -0600

    complete updates, make globals public

--- src/app/display-option/display-option.component.ts
+++ src/app/display-option/display-option.component.ts
@@ -9,7 +9,7 @@ import { Globals } from '../globals';
 })
 export class DisplayOptionComponent implements OnInit {
 
-  constructor( private globals: Globals ) { }
+  constructor( public globals: Globals ) { }
 
   ngOnInit() {
   }
